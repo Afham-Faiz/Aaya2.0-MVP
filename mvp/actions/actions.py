cec6326afe2cecbe24dda7b124feb8e6639e0d4c
Afham <hussain.faiz@student.manchester.ac.uk>
2024-11-11 20:41:08 +0000

    Aaya V5

--- mvp/actions/actions.py
+++ mvp/actions/actions.py
@@ -1,2 +1,18 @@
 # actions.py
 
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(text="I'm not sure I understand. You can try:\n"
+                               "- Asking about our products\n"
+                               "- Getting customer support\n")
+        return []
